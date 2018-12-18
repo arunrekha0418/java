4c2319fb77fd94c7b903a4f5f7d3e6ef44b0877f
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2018-12-18 11:45:44 +0530

    Create ex4aplha.java

---
+++ ex4aplha.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class Ideone
+{
+public static void main(String[] args)
+{
+char ch;
+Scanner scan=new Scanner(System.in);
+ch=scan.next(0);
+if(ch>='a' && ch<='z')
+{
+System.out.println("Alphabet");
+}
+else
+{
+System.out.println("No");
+}
+}
+}

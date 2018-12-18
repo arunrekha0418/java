95dd212160ffe816b6d25e5b0fb5e04a7a059724
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2018-12-18 11:25:03 +0530

    Update and rename aphab.java to aphabet.java

---
+++ aphabet.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class Ideone
+{
+public static void main(String[] args)
+{
+char ch;
+Scanner scan=new Scanner(System.in);
+ch=scan.next();
+if(ch>='a' && ch<='z')
+{
+System.out.println("Alphabet");
+}
+else
+{
+System.out.println("No");
+}
+}
+}
+

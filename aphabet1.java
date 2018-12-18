df89359c4671f04af256b45de48e32d79ed0ce4d
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2018-12-18 12:08:15 +0530

    Update and rename aphabet.java to aphabet1.java

--- aphabet1.java
+++ aphabet1.java
@@ -7,8 +7,8 @@ public static void main(String[] args)
 {
 char ch;
 Scanner scan=new Scanner(System.in);
-ch=scan.next(0);
-if(ch>='a' && ch<='z')
+ch=scan.next().charAt(0);
+if((ch>='a' && ch<='z')||(ch>='A'&&ch<='Z')
 {
 System.out.println("Alphabet");
 }

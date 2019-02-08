b4237088de537803d1b8b2d032c11e8528320199
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2019-02-08 23:08:09 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+public static void main(String[]args)
+{
+int i,a,b;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+for(i=a+1;i<b;i++)
+if(i%2==1)
+{
+Ssytem.out.println(i);
+}}
+}

56046eccaec7f0ba2c0e6a490a274d8e38eaca57
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2019-02-08 12:32:30 +0530

    Create power2.java

---
+++ power2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	
+	{
+		int n,k,i;
+		int result=1;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		k=sc.nextInt();
+		for(i=1;i<=k;i++)
+		{
+			result=result*n;
+		}
+			System.out.println(result);
+			
+		}
+		}
+	
+		

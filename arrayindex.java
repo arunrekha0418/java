6153abd94408f6382827dcf6e73edb1b26195900
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2019-02-17 22:12:08 +0530

    Create arrayindex.java

---
+++ arrayindex.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{ 
+		int n;
+		Scanner scan=new Scanner(System.in);
+		n=scan.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++){
+			arr[i]=scan.nextInt();
+		}
+		for(int i=0;i<=arr[i];i++){
+            System.out.println(+arr[i]+" "+i+"\n");
+        
+    }
+}
+}

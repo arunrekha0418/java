dc2752f7fb7e19d300c6d4acab77902e32757704
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2019-02-20 22:32:51 +0530

    Create median.java

---
+++ median.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,temp=0;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++){
+			for(int j=0;j<n-i-1;j++){
+				if(a[j]>a[j+1]){
+					temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;   
+				}}
+		}
+		System.out.println(a[n/2]);
+	}
+}

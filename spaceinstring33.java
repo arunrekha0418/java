b19db256aef6299fae7b9ab6759533972e9393b0
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2019-02-21 10:36:10 +0530

    Create spaceinstring33.java

---
+++ spaceinstring33.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+  
+class CountWords
+{
+    public static void main(String args[])
+    {
+        String s;
+        int count=0;
+        Scanner sc=new Scanner(System.in);
+        s=sc.nextLine();
+        for(int i=0; i<s.length()-1; i++)
+        {
+            if(s.charAt(i)==' ')
+                count++;
+        }
+        System.out.println(count);
+    }
+}

683ba65c1ebd5103406476eb73fd10b49794e2aa
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2019-02-21 09:22:52 +0530

    Create countarray.java

---
+++ countarray.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s;
+    Scanner sc=new Scanner(System.in);
+	s=sc.nextLine();
+	int count=0;
+for(int i=0;i<s.length();i++){
+	if(s.charAt(i)!=' ')
+	count++;}
+	System.out.println(count);
+	}
+}

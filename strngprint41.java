203bad2c34aa4e72ea46cc9813786ca4c56b8e49
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2019-02-21 21:08:27 +0530

    Create strngprint41.java

---
+++ strngprint41.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s;
+		int k,count=0;
+		Scanner sc=new Scanner(System.in);
+		s=sc.nextLine();
+		k=sc.nextInt();
+		for(int i=1;i<=k;i++){
+			System.out.println(s+"\n");
+				}
+}}

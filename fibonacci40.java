379b9a6624dc006ba90ff84c52032451a17876ea
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2019-02-21 20:15:51 +0530

    Create fibonacci40.java

---
+++ fibonacci40.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a=0,b=1,c,n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for(int i=0;i<=n;i++){
+			System.out.println(a);
+			c=a+b;
+			a=b;
+			b=c;
+				}
+}}

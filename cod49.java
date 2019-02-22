c1df9eef43499dce1c214b529fbb67aa8e52404e
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2019-02-22 19:11:39 +0530

    Create cod49.java

---
+++ cod49.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		int c=a+b;
+		System.out.println(c);
+	}}

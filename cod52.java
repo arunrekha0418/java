88625fc934a68a8a7b6c57b9a14cf39e230da126
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2019-02-24 23:10:07 +0530

    Create cod52.java

---
+++ cod52.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num;
+		Scanner sc=new Scanner(System.in);
+		num=sc.nextInt();
+	String str[]={" ","one","two","three","four","five","six","seven","eight","nine","ten"};
+	if((num>0)&&(num<=10))
+		System.out.println(str[num]);}
+}

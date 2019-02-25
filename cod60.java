b83abc63bd236effd424ace417a954a96c4fbb13
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2019-02-25 19:16:01 +0530

    Create cod60.java

---
+++ cod60.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,temp,sum=0;
+    Scanner sc=new Scanner(System.in);
+	n=sc.nextInt();
+	for(int i=0;i<=n;i++){
+		sum=sum+i;}
+	System.out.println(sum);
+	
+}
+}

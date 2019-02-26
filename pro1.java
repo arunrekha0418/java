ddbadda4dcffd8fa64b13a98190f9dd67fa5df5a
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2019-02-26 19:00:29 +0530

    Create pro1.java

---
+++ pro1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner s=new Scanner(System.in);
+    char[] a =s.next().toCharArray();
+      char[] b =s.next().toCharArray(); 
+        for(int i=0;i<=a.length;i++)    {
+        int count=0;
+    while(a[i]!=b[i]){
+    	count++;
+    }
+    System.out.print(a[i]);
+            
+    }  
+            
+        }}
+    
+
+	

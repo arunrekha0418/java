61ff22c40186b24374f38ab31ccb141e2bdf74b0
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2019-03-04 20:09:40 +0530

    Create player4.java

---
+++ player4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 String s;
+	 Scanner sc=new Scanner(System.in);
+	 s=sc.nextLine();
+	 System.out.print(s+".");
+	}
+}

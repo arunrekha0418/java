62ce5c229dce6dfe5f9f4fabe4f89d3e3721e7e8
arunrekha0418 <42768709+arunrekha0418@users.noreply.github.com>
2019-06-14 16:33:55 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+class Hello
+{
+public static void main(String[]args)
+{
+int i,n;
+Scanner scan=new Scanner(System.in);
+n=scan.nextInt();
+for(i=0;i<n;i++)
+{
+System.out.println("Hello\t");
+}
+}
+}
